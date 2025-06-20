a4287adfc1da8bffbe2bb34b0680bd286b5bacfb
Wilson Dev <hch.wilson@hynkor.com>
2025-06-20 17:48:21 -0500

    ✨ feat: enhance TodoList component with improved state management and UI updates

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,39 +1,46 @@
-import { useState, React } from 'react'
+import { useState } from 'react'
 import { TodoForm } from './TodoForm'
 import { Todo } from './Todo'
 
 function TodoList() {
   const [todos, setTodos] = useState([])
+  const [totalTodos, setTotalTodos] = useState(0)
 
   const addTodo = (todo) => {
-    if (todo.text.trim()) {
+    if (todo.text.trim() && !todos.find(t => t.text.trim() === todo.text.trim())) {
       todo.text = todo.text.trim()
       const updatedTodos = [todo, ...todos]
       setTodos(updatedTodos)
+      setTotalTodos(updatedTodos.length)
     }
-    
   }
 
   const deleteTodo = (id) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id)
-    setTodos(updatedTodos)
+    if (todos.find(t => t.id === id)) {
+      const updatedTodos = todos.filter(todo => todo.id !== id)
+      setTodos(updatedTodos)
+      setTotalTodos(updatedTodos.length)
+    }
   }
 
   const completeTodo = (id) => {
-    const updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-    setTodos(updatedTodos)
+    if (todos.find(t => t.id == id)) {
+      const updatedTodos = todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      setTodos(updatedTodos)
+      setTotalTodos(updatedTodos.length)
+    }
   }
 
   return (
     <>
       <TodoForm onSubmit={addTodo} />
-      <div className="flex flex-wrap justify-center pt-4">
-        {
+      <div className="w-full flex flex-wrap justify-center items-center pt-4">
+        {todos.length > 0 ? (
           todos.map((todo) =>
             <Todo
               key={todo.id}
@@ -44,7 +51,25 @@ function TodoList() {
               completeTodo={completeTodo}
             />
           )
-        }
+        ) : (
+          <div>
+            <p className='text-lg text-slate-500'>No tasks</p>
+          </div>
+        )}
+      </div>
+      <div className="text-black p-4 flex justify-center space-x-4">
+        <span>Total: {totalTodos}</span>
+        <ul className="flex gap-4 justify-center">
+          <li>
+            <a href="#" className="hover:underline">All</a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">Pending</a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">Completed</a>
+          </li>
+        </ul>
       </div>
     </>
   )
